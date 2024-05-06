935c203e64dd55f7e3591f65d98eaaeb4f9af8ab
valitettavasty <2720298312@qq.com>
2024-05-06 14:13:18 +0800

    solved week2

--- week2/rwc/src/main.rs
+++ week2/rwc/src/main.rs
@@ -1,4 +1,6 @@
-use std::env;
+use std::{env, io};
+use std::fs::File;
+use std::io::BufRead;
 use std::process;
 
 fn main() {
@@ -9,4 +11,25 @@ fn main() {
     }
     let filename = &args[1];
     // Your code here :)
+    let file = File::open(filename)?;
+    let mut content_line :Vec<String> = Vec::new();
+    for line in io::BufReader::new(file).lines(){
+        let line = line?;
+        content_line.push(line);
+    }
+    let mut content_word :Vec<String> =Vec::new();
+    println!("lines:{}",content_line.len());
+    for line in content_line.iter(){
+        for word in line.split_whitespace(){
+            content_word.push(word.to_string());
+        }
+    }
+    println!("words:{}",content_word.len());
+    let mut content_char :Vec<char> = Vec::new();
+    for word in content_word.iter(){
+        for word_char in word.chars(){
+            content_char.push(word_char);
+        }
+    }
+    println!("chars:{}",content_char.len());
 }
